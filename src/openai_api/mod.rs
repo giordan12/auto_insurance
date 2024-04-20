9a2073f74afb49ec7b9988e2bd291978cfe8149e
Gio Castro <giordan12@me.com>
2024-04-20 12:17:14 -0500

    Adding directory where I can write an interface to the openai api

---
+++ src/openai_api/mod.rs
@@ -0,0 +1,6 @@
+// Here is where we will define a basic rust api to reach out to the chat completion api
+// of openai as explained here: https://platform.openai.com/docs/api-reference/chat/create
+// From what I see we want to define a basic url, a way to pass the authorization token,
+// specify the model to use, a system prompt and the user initial chat. Then we want to be able
+// to handle the output. From this I think we only want the content of the response for now at least.
+//
